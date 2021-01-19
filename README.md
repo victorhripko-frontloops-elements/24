90684405632fa9c95d7f4e0409d392109ed2639f
Victor Hripko <hripko.v@soft2bet.com>
2021-01-19 16:27:21 +0200

    toasts

--- README.md
+++ README.md
@@ -1,21 +1,7 @@
-# Template for FrontLoops [Elements] challenge
+# Toast
 
-Install all dependencies
-```bash
-npm i
-```
-
-For start watching all files use:
-```bash
-npm run start
-```
-
-then open in browser
-```bash
-http://localhost:1234/
-```
-
-For build all files use:
-```bash
-npm run build
-```
+Build a component that shows a toast (notification) with animation on button click.
+Each toast can have its own message and type (default or error).
+Toasts should hide automatically after 1.5 seconds.
+If toast is hovered by mouse - timer should reset and start over again when cursor leaves toast.
+While one toast is active showing other toasts should be blocked.
--- src/index.js
+++ src/index.js
@@ -2,4 +2,66 @@ import './style.scss';
 
 (() => {
 
+  class Toast {
+    constructor(text, modifier, callback) {
+      this.text = text || 'Default message';
+      this.modifier = modifier;
+      this.callback = callback;
+      this.toast;
+      this.timeout;
+
+      this.renderToast();
+    };
+
+    renderToast() {
+      this.toast = document.createElement('div');
+
+      this.toast.className = 'toast';
+      this.toast.innerHTML = this.text;
+
+      if ( this.modifier ) {
+        this.toast.classList.add(`toast--${this.modifier}`);
+      };
+
+      document.body.append(this.toast);
+
+      this.toast.addEventListener('mouseenter', () => clearTimeout(this.timeout));
+      this.toast.addEventListener('animationend', () => this.startTimeout());
+      this.toast.addEventListener('mouseleave', () => this.startTimeout());
+    };
+
+    startTimeout() {
+      this.timeout = setTimeout(() => {
+        this.toast.classList.add('animation-remove');
+        this.toast.addEventListener('animationend', () => this.removeToast());
+      }, 1500);
+    };
+
+    removeToast() {
+      this.toast.remove();
+
+      if ( typeof this.callback === 'function' ) {
+        this.callback();
+      }
+    };
+  };
+
+  const messageToastButtons = document.querySelectorAll('[data-toast="message"]');
+  const alertToastButtons = document.querySelectorAll('[data-toast="danger"]');
+
+  let alreadyToast = false;
+
+  messageToastButtons.forEach((btn) => btn.addEventListener('click', () => {
+    if ( alreadyToast ) return;
+    new Toast(null, null, () => alreadyToast = false);
+    alreadyToast = true;
+  }));
+
+  alertToastButtons.forEach((btn) => btn.addEventListener('click', () => {
+    if ( alreadyToast ) return;
+    new Toast(null, 'danger', () => alreadyToast = false);
+    alreadyToast = true;
+  }));
+
+
 })();
